a0efabcf04975bccdaf307cf9661260f44d9abd9
ArthurTavares26 <arthurtvss26@gmail.com>
2026-06-02 07:45:07 -0300

    Update estudoProva

--- trabalho.js
+++ trabalho.js
@@ -1,17 +1,91 @@
-let nomeDoAluno = prompt("Digite seu nome: ");
-let frequencia = parseInt(prompt("Digite qual foi sua frequência: "));
-let notaAv1 = parseFloat(prompt("Digite sua nota na AV1: "));
-let notaAv2 = parseFloat(prompt("Digite sua nota na AV2: "));
-let calcularParcial = (notaAv1 + notaAv2)/2;
-let notaAv3;
-if(calcularParcial >= 4){
-    notaAv3 = parseFloat(prompt("Digite sua nota da AV3: "));
-    let notaFinal = (calcularParcial + notaAv3)/2;
-    if((notaFinal >= 5) && (frequencia >= 75)){
-    console.log("Parabéns", nomeDoAluno, "você foi aprovado e sua nota final foi:", notaFinal);
-} else {
-    console.log("Reprovado na Nota Final ou na frequência.");
+1° QUESTÃO://
+function mediaAprovados(notas) {
+    let soma = 0;
+    let quantidade = 0;
+
+    for (let i = 0; i < notas.length; i++) {
+        if (notas[i] >= 6) {
+            soma += notas[i];
+            quantidade++;
+        }
+    }
+
+    if (quantidade === 0) {
+        return "Nenhum aprovado";
+    }
+
+    return soma / quantidade;
 }
-} else {
-    console.log("Reprovado na Nota Parcial.");
+
+console.log(mediaAprovados([4, 7, 9, 5, 6]));
+console.log(mediaAprovados([2, 3, 5]));
+
+
+
+
+2° QUESTÃO:
+const temperaturas = [18, 22, 27, 31, 29, 24, 19];
+
+let maior = temperaturas[0];
+let menor = temperaturas[0];
+let contadorCalor = 0;
+
+for (let i = 0; i < temperaturas.length; i++) {
+
+    if (temperaturas[i] > maior) {
+        maior = temperaturas[i];
+    }
+
+    if (temperaturas[i] < menor) {
+        menor = temperaturas[i];
+    }
+
+    if (temperaturas[i] >= 28) {
+        contadorCalor++;
+    }
 }
+
+console.log("Maior temperatura:", maior);
+console.log("Menor temperatura:", menor);
+console.log("Temperaturas igual ou superior a 28:", contadorCalor);
+
+
+
+
+3° QUESTÃO: 
+function turmaComMaiorMedia(turmas) {
+
+    let maiorMedia = 0;
+    let indiceMaior = 0;
+
+    for (let i = 0; i < turmas.length; i++) {
+
+        let soma = 0;
+
+        for (let j = 0; j < turmas[i].length; j++) {
+            soma += turmas[i][j];
+        }
+
+        let media = soma / turmas[i].length;
+
+        if (media > maiorMedia) {
+            maiorMedia = media;
+            indiceMaior = i;
+        }
+    }
+
+    return indiceMaior;
+}
+
+const turmas = [
+    [7, 8, 5],
+    [4, 3, 6, 9],
+    [10, 7]
+];
+
+console.log(turmaComMaiorMedia(turmas));
+
+
+
+
+
